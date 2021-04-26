98acd5ee0cf670dbb7127b982acda0258159c1cc
Toumi Atef <yahyatoumii@gmail.com>
2021-04-26 19:59:05 +0200

    Correction

--- DET/Assets/Scripts/PlayerMovement.cs
+++ DET/Assets/Scripts/PlayerMovement.cs
@@ -21,41 +21,8 @@ public class PlayerMovement : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    void Update()
+    void FixedUpdate() 
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
-
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
-
-        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-        float rayLength;
-
-        if(groundPlane.Raycast(cameraRay, out rayLength))
-        {
-            Vector3 pointToLookAt = cameraRay.GetPoint(rayLength);
-            transform.LookAt(new Vector3(pointToLookAt.x, transform.position.y, pointToLookAt.z));
-        }
-
         if(Input.GetKey(KeyCode.Z) )
         {
             anim.SetBool("IsRunning",true);
@@ -91,5 +58,44 @@ public class PlayerMovement : MonoBehaviour
         {
             anim.SetBool("Right", false);
         }
+    }
+
+    void Update()
+    {
+
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+        // Changes the height position of the player..
+        if (Input.GetKeyDown(KeyCode.Space) && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+        
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLookAt = cameraRay.GetPoint(rayLength);
+            transform.LookAt(new Vector3(pointToLookAt.x, transform.position.y, pointToLookAt.z));
+        }
+
     }
 }
