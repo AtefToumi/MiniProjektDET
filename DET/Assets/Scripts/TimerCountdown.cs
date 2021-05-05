38f3c30277b9cd31e2e7987656863aec0e7f9bcb
Toumi Atef <yahyatoumii@gmail.com>
2021-05-05 16:13:18 +0200

    timer added

---
+++ DET/Assets/Scripts/TimerCountdown.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimerCountdown : MonoBehaviour
+{
+    Text text;
+    public int minutesLeft = 5;
+    float timeLeft;
+    public bool takingAway = false;
+
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        timeLeft = minutesLeft * 60;
+    }
+
+    void Update()
+    {
+        if(takingAway == false && timeLeft > 0)
+        {
+            StartCoroutine(TimerTake());
+        }
+    }
+
+    IEnumerator TimerTake()
+    {
+        takingAway = true;
+        yield return new WaitForSeconds(1);
+        timeLeft -= 1;
+        TimeSpan time = TimeSpan.FromSeconds(timeLeft);
+        text.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
+        takingAway = false;
+    }
+}
