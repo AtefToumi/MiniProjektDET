86aa744329574adc2fbe955c954d58c5571703ed
roula02 <roula.alhajar95@gmail.com>
2021-05-08 02:18:58 +0200

    fixing setDestination-Error

--- DET/Assets/Scripts/ZombieScripts/ZombieMovement.cs
+++ DET/Assets/Scripts/ZombieScripts/ZombieMovement.cs
@@ -4,14 +4,15 @@ using UnityEngine.AI;
 
 public class ZombieMovement : MonoBehaviour
 {
-   Transform player;
+     Transform player;
     ZombieHealth zombieHealth;
+    
 
    NavMeshAgent nav;
    ZombieAttack zombieAttack;
 
    void Awake() {
-       player = GameObject.FindGameObjectWithTag("Player").transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
        nav = GetComponent<NavMeshAgent>();
        zombieHealth = GetComponent<ZombieHealth>();
        zombieAttack = GetComponent<ZombieAttack>();
@@ -20,15 +21,16 @@ public class ZombieMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!zombieHealth.isDead && !zombieAttack.playerInRange)
+        if(!zombieHealth.isDead )//&& !zombieAttack.playerInRange)
         {
-            nav.SetDestination(player.position);
+              nav.SetDestination(player.position);
+         
         }
         
-       else
+      else
         {
             nav.enabled = false;
-        }
+        } 
         
-    }
+    } 
 }
