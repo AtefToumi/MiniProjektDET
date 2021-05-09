dc86e8dd229f6a4d28b94c0fffa825520bf8c691
roula02 <roula.alhajar95@gmail.com>
2021-05-09 16:10:44 +0200

    Stone move

---
+++ DET/Assets/Cubemove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubemove : MonoBehaviour
+{
+    Animator anim;
+    public GameObject player;
+    Transform transform;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        player.transform.position = GetComponent<Transform>().position; 
+        
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("collision detected");
+            anim.SetBool("move", true);
+            // player.transform.parent = other.gameObject.transform;
+            this.transform.position = player.transform.position;
+
+
+           
+
+        }
+    }
+}
