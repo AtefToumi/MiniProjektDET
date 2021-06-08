44c1e1f3f1304726b845744190c5aa06c2edfbae
Atef <yahyatoumii@gmail.com>
2021-06-09 01:11:31 +0200

    added Flamethrower VFX

---
+++ DET/Assets/Flamethrower.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Flamethrower : MonoBehaviour
+{
+    public VisualEffect flamethrower;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flamethrower.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            flamethrower.Play();
+        }
+
+        if(Input.GetMouseButtonUp(1))
+        {
+            flamethrower.Stop();
+        }
+    }
+}
