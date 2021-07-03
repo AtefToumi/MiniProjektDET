04004ce1e917a69c6cac1d3928b1d70f22066657
Atef <yahyatoumii@gmail.com>
2021-07-03 19:32:46 +0200

    Weapon Aim added

--- DET/Assets/Scripts/PlayerAiming.cs
+++ DET/Assets/Scripts/PlayerAiming.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Animations.Rigging;
 public class PlayerAiming : MonoBehaviour
 {
     public float turnSpeed = 5;
     Camera mainCamera;
+    public Rig aimLayer;
+    public float aimDuration = 0.3f;
     
     // Start is called before the first frame update
     void Start()
@@ -20,5 +22,16 @@ public class PlayerAiming : MonoBehaviour
     {
         float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
+    }
+
+    void Update() 
+    {
+        if(Input.GetMouseButton(1))
+        {
+            aimLayer.weight += Time.deltaTime / aimDuration;
+        } else
+        {
+            aimLayer.weight -= Time.deltaTime / aimDuration;
+        }
     }
 }
