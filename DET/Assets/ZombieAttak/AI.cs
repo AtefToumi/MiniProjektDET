33560d37e4ea8630ba3950ac89aa1d537a3d2656
roula02 <roula.alhajar95@gmail.com>
2021-07-14 15:22:50 +0200

    fehlerhafte KI

---
+++ DET/Assets/ZombieAttak/AI.cs
@@ -0,0 +1,155 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public float health;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject grenade;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    //Animator
+    Animator anim;
+
+    //Player Score
+    public int scoreValue = 10;
+
+    private void Awake()
+    {
+       // player = GameObject.FindGameObjectWithTag("Player").transform ;
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange)
+        {
+            anim.SetBool("InAttackRange", false);
+            anim.SetBool("InSightRange", false);
+
+            Patroling();
+        }
+        if (playerInSightRange && !playerInAttackRange)
+        {
+            anim.SetBool("InSightRange", true);
+            anim.SetBool("InAttackRange", false);
+
+            ChasePlayer();
+        }
+        if (playerInAttackRange && playerInSightRange)
+        {
+        
+            anim.SetBool("InAttackRange", true);
+            AttackPlayer();
+        }
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet)
+        {
+         SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;     
+    }
+
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+
+    // Attack
+    private void AttackPlayer()
+    {
+        //enemy cann't move during Attack
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            //Instantiate the Grenade 
+            Rigidbody rb = Instantiate(grenade, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage)   //TO-DO : wird noch nicht woanders aufgerufen 
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Invoke(nameof(DestroyEnemy), 0.5f);
+        }
+    }
+    private void DestroyEnemy()
+    {
+        ScoreManager.score += scoreValue;
+        anim.SetTrigger("Dead");
+        Destroy(gameObject,6f);
+    }
+
+   /* private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    } */
+}
---
+++ DET/Assets/ZombieAttak/grenade.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grenade : MonoBehaviour
+{
+  //  public GameObject ExplosionEffect;  //TO-DO : diese Effect erstellen 
+
+    PlayerHealth playerHealth;
+
+    public int grenadeDamage = 20;
+
+
+    public void OnCollisionEnter(Collision other)
+    {
+     //   Instantiate(ExplosionEffect, transform.position, transform.rotation);
+
+        if (other.gameObject.tag == "Player")
+        {
+            if (playerHealth.currentHealth > 0)
+            {
+                playerHealth.TakeDamage(grenadeDamage);
+            }
+        }
+
+
+      //  Destroy(this.gameObject);
+    }
+
+} 
